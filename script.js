c643390a1af64588e4695b826864ab0f9acd7e82
Atul Raj <raazatul7@gmail.com>
2018-10-25 00:26:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//BlackJack Game
+// By Atul Raj
+let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+let values = [
+  "Ace",
+  "King",
+  "Queen",
+  "Jack",
+  "Ten",
+  "Nine",
+  "Eight",
+  "Seven",
+  "Six",
+  "Five",
+  "Four",
+  "Three",
+  "Two",
+  "One"
+];
+let deck = [];
+
+let playerCards = [deck[0], deck[2]];
+
+console.log("Welcome to BlackJack");
+
+console.log("You are Dealt");
+console.log(playerCards[0]);
+console.log(playerCards[1]);
